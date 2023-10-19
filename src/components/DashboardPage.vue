8252f71565715f17774a1cb6abe8f7746bede4f8
Henri du Plessis <Henri.duPlessis@psg.co.za>
2023-10-19 23:08:53 +0200

    Fixed continue/view details buttons

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -1,117 +1,87 @@
 <template>
+	<div>
 
-		<div >
+		<v-container>
 
-			<v-container>
+			<v-table>
+				<thead>
+					<tr>
+						<td colspan="4">
+							<h1>{{ name }}</h1>
+						</td>
 
-				<v-table>
-					<thead>
-						<tr>
-							<td colspan="4">
-								<h1>{{ name }}</h1>
-							</td>
+					</tr>
+				</thead>
 
-						</tr>
-					</thead>
+				<tbody>
+					<tr>
+						<td><b>Location</b></td>
+						<td>{{ location }}</td>
+						<td><b>Industry</b></td>
+						<td>{{ industry }}</td>
+					</tr>
+					<tr>
+						<td><b>Size</b></td>
+						<td>{{ size }}</td>
 
-					<tbody>
-						<tr>
-							<td><b>Location</b></td>
-							<td>{{ location }}</td>
-							<td><b>Industry</b></td>
-							<td>{{ industry }}</td>
-						</tr>
-						<tr>
-							<td><b>Size</b></td>
-							<td>{{ size }}</td>
+						<td><b>Description</b></td>
+						<td>{{ description }}</td>
+					</tr>
+				</tbody>
+			</v-table>
 
-							<td><b>Description</b></td>
-							<td>{{ description }}</td>
-						</tr>
-					</tbody>
-				</v-table>
+		</v-container>
+		<v-container>
 
-			</v-container>
-			<v-container>
-
-				<v-table>
-					<thead style="margin: 1rem 0rem 1rem 0rem;">
-						<tr>
-							<td colspan="5">
-								<h1>ESG Reports</h1>
-							</td>
-							<td style="text-align: right;">
-								<v-btn color="#219653" size="small" @click="createNewReport">
-									New Report
-								</v-btn>
-							</td>
-						</tr>
-
-
-					</thead>
-				</v-table>
-				<v-table>
-					<tbody style="text-align: center;">
-						<tr style="font-size: smaller;">
-							<td>Submission ID</td>
-							<td>Year</td>
-							<td>Submitter</td>
-							<td>Submission Date</td>
-							<td>Status</td>
-							<td></td>
-						</tr>
-						<tr v-for="submission in submissions" :key="submission.SubmissionID">
-							<td>{{ submission.SubmissionID }}</td>
-							<td>{{ submission.Year }}</td>
-							<td>{{ submission.FirstName }} {{ submission.LastName }}</td>
-							<td>{{ new Date(submission.Date).toLocaleDateString() }}</td>
-							<td>{{ getStatusText(submission.Status) }}</td>
-							<td>
-								<v-btn v-if="submission.Status === 0" color="green">Continue</v-btn>
-								<v-btn v-else color="blue">View Details</v-btn>
-							</td>
-						</tr>
+			<v-table>
+				<thead style="margin: 1rem 0rem 1rem 0rem;">
+					<tr>
+						<td colspan="5">
+							<h1>ESG Reports</h1>
+						</td>
+						<td style="text-align: right;">
+							<v-btn color="#219653" size="small" @click="createNewReport">
+								New Report
+							</v-btn>
+						</td>
+					</tr>
 
 
-						<!-- <tr>
-							<td>PLUI7999765GH2023</td>
-							<td>2022</td>
-							<td>03/02/2023</td>
-							<td>In Progress</td>
-							<td><v-btn color="green">Continue</v-btn></td>
-							<td>NA</td>
-						</tr>
-						<tr>
-							<td>PLUI7999765GH2023</td>
-							<td>2022</td>
-							<td>03/02/2023</td>
-							<td>In Progress</td>
-							<td><v-btn color="blue">View Details</v-btn></td>
-							<td>NA</td>
-						</tr>
-						<tr>
-							<td>PLUI7999765GH2023</td>
-							<td>2022</td>
-							<td>03/02/2023</td>
-							<td>In Progress</td>
-							<td><v-btn color="blue">View Details</v-btn></td>
-							<td>NA</td>
-						</tr>
-						<tr>
-							<td>PLUI7999765GH2023</td>
-							<td>2022</td>
-							<td>03/02/2023</td>
-							<td>In Progress</td>
-							<td><v-btn color="blue">View Details</v-btn></td>
-							<td>NA</td>
-						</tr> -->
-					</tbody>
-				</v-table>
+				</thead>
+			</v-table>
+			<v-table>
+				<tbody style="text-align: center;">
+					<tr style="font-size: smaller;">
+						<td>Submission ID</td>
+						<td>Year</td>
+						<td>Submitter</td>
+						<td>Submission Date</td>
+						<td>Status</td>
+						<td></td>
+					</tr>
+					<tr v-for="submission in submissions" :key="submission.SubmissionID">
+						<td>{{ submission.SubmissionID }}</td>
+						<td>{{ submission.Year }}</td>
+						<td>{{ submission.FirstName }} {{ submission.LastName }}</td>
+						<td>{{ new Date(submission.Date).toLocaleDateString() }}</td>
+						<td>{{ getStatusText(submission.Status) }}</td>
+						<td>
+							<v-btn v-if="submission.Status === 0" color="green"
+								@click="goToCreateNewReport(submission.SubmissionID)">
+								Continue
+							</v-btn>
+							<v-btn v-else color="blue" @click="goToSuccessPage(submission.SubmissionID)">
+								View Details
+							</v-btn>
+						</td>
+					</tr>
+				</tbody>
+			</v-table>
 
 
 
-			</v-container>
-		</div>
+		</v-container>
+	</div>
 </template>
   
 <script>
@@ -136,6 +106,14 @@ export default {
 		this.fetchDashboardData();
 	},
 	methods: {
+		goToCreateNewReport(submissionID) {
+			console.log(submissionID)
+			this.$router.push({ name: 'CreateNewReport', query: { submissionID } });
+		},
+		goToSuccessPage(submissionID) {
+			console.log(submissionID)
+			this.$router.push({ name: 'SuccessPage', query: { submissionID } });
+		},
 		async fetchDashboardData() {
 			try {
 				const token = localStorage.getItem('access_token');
@@ -163,32 +141,30 @@ export default {
 		},
 		async createNewReport() {
 			const token = localStorage.getItem('access_token');
-				const headers = {
-					'Authorization': 'Bearer ' + token
-				};
+			const headers = {
+				'Authorization': 'Bearer ' + token
+			};
 
-				const response = await axios.get(config.backendApiUrl.concat("/start_submission"), { headers: headers });
-				if (response.data.success) {
-					const SubmissionID = response.data.submission_id;
-                    this.$router.push({ name: 'CreateNewReport', query: { submissionID: SubmissionID }});
-				}
+			const response = await axios.get(config.backendApiUrl.concat("/start_submission"), { headers: headers });
+			if (response.data.success) {
+				const SubmissionID = response.data.submission_id;
+				this.$router.push({ name: 'CreateNewReport', query: { submissionID: SubmissionID } });
+			}
 		},
 
-	
+
 		getStatusText(status) {
-        switch (status) {
-            case 0: return 'In Progress';
-            case 1: return 'Complete';
-            case 2: return 'Rejected';
-            default: return 'Unknown';
-        }
-    }
+			switch (status) {
+				case 0: return 'In Progress';
+				case 1: return 'Complete';
+				case 2: return 'Rejected';
+				default: return 'Unknown';
+			}
+		}
 	}
 }
 
 </script>
   
-<style scoped>
-
-</style>
+<style scoped></style>
   
